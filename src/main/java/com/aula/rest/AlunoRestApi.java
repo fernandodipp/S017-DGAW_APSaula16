c17847d09536ce27529e6f41ef127064310d9d91
fernandodipp <fernando.dipp@gmail.com>
2021-07-01 19:14:23 -0300

    limpando projeto

--- src/main/java/com/aula/rest/AlunoRestApi.java
+++ src/main/java/com/aula/rest/AlunoRestApi.java
@@ -13,11 +13,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.aula.exception.ValidacaoException;
 import com.aula.modelo.AlunoModelo;
 import com.aula.negocio.AlunoServico;
 
-import com.aula.exception.EntidadeNaoEncontradaException;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
